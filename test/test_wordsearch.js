74ffd72e12948cffef98fa86ef2a832b71a6f933
Alex Thon <ghaiquol@gmail.com>
2021-11-03 21:51:57 +0000

    Add wordsearch.js and test_wordsearch.js work done by both me and Ekin

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,9 +1,32 @@
 const chai = require('chai');
 const assert = chai.assert;
+// edgecases: No input
+// edgecase 3: empty array
+//edgecase 4: if the word is vertical
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
+  it("Should return false if no word or empty array", () => {
+    const result = wordSearch([],'');
+    assert.isFalse(result);
+  });
+  it("should return true if the word is vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYHW');
+
+    assert.isTrue(result);
+
+  });
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -15,7 +38,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +54,11 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
 });
+
+
+// pair-gramming team: Alex Thon and Ekin Koluk
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,56 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  // we will return an array
+  let transposed = [];
 
-module.exports = wordSearch
+  for (let k = 0; k < matrix[0].length; k++) { //rows
+    let col = [];
+    for (let j in matrix) { //columns
+      col.push(matrix[j][k]);
+    }
+    transposed.push(col);
+  }
+  return transposed;
+};
+
+
+
+const wordSearch = (letters, word) => {
+  if (!letters || !word) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word))  {
+      return true;
+    }
+  }
+  let newArr = transpose(letters);
+  const verticalJoin = newArr.map(arr => arr.join(''));
+  for (let a of verticalJoin) {
+    if (a.includes(word))  {
+      return true;
+    }
+  }
+
+  return false;
+    
+};
+
+
+const result = wordSearch([
+  ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'FRANK');
+console.log(result);
+
+  
+module.exports = wordSearch;
+
+
